37c0202b631c9a89643333871f91e66fbd2f225a
Tomohiro Ichimura <tomohiro.ichimura@gmail.com>
2021-07-04 10:50:11 +0000

    update data as of 20210704

--- japan_covid19_data.js
+++ japan_covid19_data.js
@@ -1,5 +1,5 @@
 const japandata = {
-  "update": 2021/07/03 19:50, 
+  "update": 2021/07/04 19:50, 
   "type": "FeatureCollection",
   "features": [
     {
@@ -13,7 +13,7 @@ const japandata = {
         "capital_name": "Sapporo",
         "iso_3166-2": "JP-01",
         "feature_id": 70258,
-        "count": 41397
+        "count": 41427
       },
       "geometry": {
         "type": "Point",
